45039dc1c493a6a5ef6de3da76de93de296c1750
MrLokans <trikster1911@gmail.com>
2015-11-24 22:08:30 +0300

    Lots of fixes to correct project behaviour

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,6 +1,4 @@
-import unittest
-
-from django.test import LiveServerTestCase, TestCase
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -9,6 +7,7 @@ class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
@@ -19,66 +18,99 @@ class NewVisitorTest(LiveServerTestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
-    def test_can_start_a_list_and_retreive_it_later(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
         self.browser.get(self.live_server_url)
+
+        # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
+        # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-            )
-        inputbox.send_keys("Buy a jug of milk.")
-        user_list_url = self.browser.current_url
-        inputbox.send_keys(Keys.ENTER)
-        # self.assertRegex(user_list_url, '/lists/.+')
-        self.check_for_row_in_list_table('1: Buy a jug of milk.')
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+        )
 
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+
+        # When she hits enter, she is taken to a new URL,
+        # and now the page lists "1: Buy peacock feathers" as an item in a
+        # to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys("Drink a jug of milk.")
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
+        # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        self.check_for_row_in_list_table('2: Drink a jug of milk.')
-        self.check_for_row_in_list_table('1: Buy a jug of milk.')
-
-        # Now a new user Francine visits the site
+        # Now a new user, Francis, comes along to the site.
 
         ## We use a new browser session to make sure that no information
-        ## from the previous user is coming through from cookies, LocalStorage etc
+        ## of Edith's is coming through from cookies etc
         self.browser.quit()
         self.browser = webdriver.Firefox()
 
-        # Francine visits the home page. There is not sign of the previously created list
+        # Francis visits the home page.  There is no sign of Edith's
+        # list
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('Buy a jug of milk.', page_text)
-        self.assertNotIn('Drink a jug', page_text)
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
 
-        # Francine starts a new list by entering a new item. 
+        # Francis starts a new list by entering a new item. He
+        # is less interesting than Edith...
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
 
-        # Francine gets her unique URL
-        francine_list_url = self.browser.current_url
-        self.assertRegex(francine_list_url, '/lists/.+')
-        self.assertNotEqual(francine_list_url, user_list_url)
+        # Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
 
-        # No trace of previous user
+        # Again, there is no trace of Edith's list
         page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('Buy a jug of milk.', page_text)
-        self.assertNotIn('Drink a jug', page_text)
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # Satisfied, they both go back to sleep
+
 
     def test_layout_and_styling(self):
-        # Andrew goes to the home page
+        # Edith goes to the home page
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
 
-        # He notices the input box is centered
+        # She notices the input box is nicely centered
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('testing\n')
         self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=5)
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
+
+        # She starts a new list and sees the input is nicely
+        # centered there too
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
+
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -7,7 +7,73 @@ from lists.views import home_page
 from lists.models import Item, List
 
 
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        # resolve's django func to determine which func is called
+        # when requesting a URL
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
+
+
+class NewListTest(TestCase):
+    def test_saving_a_POST_request(self):
+        self.client.post(
+            '/lists/new',
+            data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post(
+            '/lists/new',
+            data={'item_text': 'A new list item'})
+        new_list = List.objects.first()
+        self.assertRedirects(response, '/lists/{list_id}/'.format(list_id=new_list.id))
+
+
+class NewItemTest(TestCase):
+
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            '/lists/{}/add_item'.format(correct_list.id),
+            data={'item_text': 'A new item for an existing list'}
+            )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            '/lists/{}/add_item'.format(correct_list.id),
+            data={'item_text': 'A new item for an existing list'}
+            )
+        self.assertRedirects(response, '/lists/{}/'.format(correct_list.id))
+
+
+
+
 class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        correct_list = List.objects.create()
+        response = self.client.get('/lists/{}/'.format(correct_list.id))
+        self.assertTemplateUsed(response, 'list.html')
 
     def test_passes_correct_list_to_template(self):
         other_list = List.objects.create()
@@ -15,11 +81,6 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/{}/'.format(correct_list.id))
         self.assertEqual(response.context['list'], correct_list)
 
-    def test_uses_list_template(self):
-        correct_list = List.objects.create()
-        Item.objects.create(text="item 1", list=correct_list)
-        response = self.client.get('/lists/{}/'.format(correct_list.id))
-        self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_only_items_for_that_list(self):
         correct_list = List.objects.create()
@@ -37,50 +98,6 @@ class ListViewTest(TestCase):
 
         self.assertNotContains(response, 'other item 1')
         self.assertNotContains(response, 'other item 2')
-
-
-class NewListTest(TestCase):
-
-    def test_can_save_a_POST_request_to_an_existing_list(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        self.client.post(
-            '/lists/{}/add_item'.format(correct_list.id),
-            {'item_text': 'A new item for an existing list'}
-            )
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new item for an existing list')
-        self.assertEqual(new_item.list, correct_list)
-
-    def test_redirects_to_list_view(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        response = self.client.post(
-            '/lists/{}/add_item'.format(correct_list.id),
-            {'item_text': 'A new item for an existing list'}
-            )
-        self.assertRedirects(response, '/lists/{}/'.format(correct_list.id))
-
-
-    def test_saving_a_POST_request(self):
-        self.client.post(
-            '/lists/new',
-            data={'item_text': 'A new list item'})
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_redirects_after_POST(self):
-        response = self.client.post(
-            '/lists/new',
-            data={'item_text': 'A new list item'})
-        new_list = List.objects.first()
-        self.assertRedirects(response, '/lists/{list_id}/'.format(list_id=new_list.id))
-
 
 class ListAndItemModelTest(TestCase):
 
@@ -112,16 +129,4 @@ class ListAndItemModelTest(TestCase):
 
 
 
-class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        # resolve's django func to determine which func is called
-        # when requesting a URL
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
