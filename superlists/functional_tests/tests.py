13b6d6ce3ad8aab3cfdc0d73eb4f445bee1fc522
MrLokans <trikster1911@gmail.com>
2015-11-24 22:28:58 +0300

    Added bootstrap.

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,9 +1,9 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -89,7 +89,6 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
-
 
     def test_layout_and_styling(self):
         # Edith goes to the home page
