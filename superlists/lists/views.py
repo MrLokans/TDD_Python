5ebdae411c734f2b4f635a3b3f9652feb88ba0e9
MrLokans <trikster1911@gmail.com>
2015-12-01 20:10:03 +0300

    view_list view now handles item addition.

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -15,19 +15,15 @@ def new_list(request):
         item.full_clean()
         item.save()
     except ValidationError:
-        print("ValidationError!")
         list_.delete()
         error = "You can't have an empty list item"
         return render(request, 'home.html', {"error": error})
     return redirect('/lists/%d/' % (list_.id,))
 
 
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/%d/' % (list_.id,))
-
-
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect('/lists/{}/'.format(list_.id))
     return render(request, 'list.html', {'list': list_})
