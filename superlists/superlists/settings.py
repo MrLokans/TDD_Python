670e1da2e5d83c0cc1d51460723be67b0cc38ffa
MrLokans <trikster1911@gmail.com>
2015-12-01 20:01:00 +0300

    Several validations against empy list submitting.

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'cewh@mjx_+*gn+r1-=9lc1n48t5_eu*5@0g16)xqh_+e9-o=#k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = [] #['127.0.0.1:8000']
 
 
 # Application definition
