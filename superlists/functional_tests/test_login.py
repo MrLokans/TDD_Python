08ae896e1b483bb317d5220401083b5784dc25ea
MrLokans <trikster1911@gmail.com>
2015-12-09 20:11:28 +0300

    Form ids changed

--- superlists/functional_tests/test_login.py
+++ superlists/functional_tests/test_login.py
@@ -11,7 +11,7 @@ class LoginTest(FunctionalTest):
 
         # User clicks 'Sign in' link for the first time
         self.browser.get(self.server_url)
-        self.browser.find_element_by_id('login').click()
+        self.browser.find_element_by_id('id_login').click()
 
         # Persona login forms appears
         self.switch_to_new_window('Mozilla Persona')
@@ -24,7 +24,7 @@ class LoginTest(FunctionalTest):
         self.switch_to_new_window('To-Do')
 
         # User now sees that he is logged in
-        self.wait_for_element_with_id('logout')
+        self.wait_for_element_with_id('id_logout')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn('user@mockmyid.com', navbar.text)
 
