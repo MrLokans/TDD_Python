8851ec52b89589707264a584a0bc4a31ea6640d2
MrLokans <trikster1911@gmail.com>
2015-12-11 07:16:45 +0300

    Login view extended.

--- superlists/accounts/tests/test_views.py
+++ superlists/accounts/tests/test_views.py
@@ -1,11 +1,25 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from unittest.mock import patch
+User = get_user_model()
 
 
 class LoginViewTest(TestCase):
 
     @patch('accounts.view.autheticate')
     def test_calls_authenticate_with_assertion_from_post(self, mock_authenticate):
-        mock_authenticate.return_vale = None
+        mock_authenticate.return_value = None
         self.client.post('/accounts/login', {'assertion': 'assert this'})
         mock_authenticate.assert_called_once_with(assertion='assert this')
+
+        self.assertTrue(True)
+
+    @patch('accounts.views.autheticate')
+    def test_returns_OK_when_user_found(
+        self, mock_authenticate
+    ):
+        user = User.objects.create(email='a@a.a')
+        user.backend = ''  # required for auth_login to work
+        mock_authenticate.return_value = user
+        response = self.client.post('/accounts/login', {'assertion': 'a'})
+        self.assertEqual(response.content.decode(), 'OK')
--- superlists/accounts/views.py
+++ superlists/accounts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.http import HttpResponse
+
+
+def persona_login(request):
+    authenticate(assertion=request.POST['assertion'])
+    return HttpResponse()
 
 # Create your views here.
