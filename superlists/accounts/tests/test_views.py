573527249a2828cd08501b7a466f81faedf91e53
MrLokans <trikster1911@gmail.com>
2015-12-12 12:03:43 +0300

    Typos fixed.

--- superlists/accounts/tests/test_views.py
+++ superlists/accounts/tests/test_views.py
@@ -6,7 +6,7 @@ User = get_user_model()
 
 class LoginViewTest(TestCase):
 
-    @patch('accounts.view.autheticate')
+    @patch('accounts.views.authenticate')
     def test_calls_authenticate_with_assertion_from_post(self, mock_authenticate):
         mock_authenticate.return_value = None
         self.client.post('/accounts/login', {'assertion': 'assert this'})
@@ -14,7 +14,7 @@ class LoginViewTest(TestCase):
 
         self.assertTrue(True)
 
-    @patch('accounts.views.autheticate')
+    @patch('accounts.views.authenticate')
     def test_returns_OK_when_user_found(
         self, mock_authenticate
     ):
@@ -24,16 +24,16 @@ class LoginViewTest(TestCase):
         response = self.client.post('/accounts/login', {'assertion': 'a'})
         self.assertEqual(response.content.decode(), 'OK')
 
-    @patch('accounts.views.autheticate')
+    @patch('accounts.views.authenticate')
     def test_gets_logged_in_session_if_authenticate_returns_a_user(self, mock_authenticate):
         user = User.objects.create(email='a@a.a')
         user.backend = ''
         mock_authenticate.return_value = user
         self.client.post('/accounts/login', {'assertion': 'a'})
-        self.assertEqual(self.client.session[SESSION_KEY], user.pk)
+        self.assertEqual(self.client.session[SESSION_KEY], str(user.pk))
         # .pk - is the PrimaryKey for the user model
 
-    @patch('accounts.views.autheticate')
+    @patch('accounts.views.authenticate')
     def test_does_not_get_logged_in_if_auth_returns_None(self, mock_authenticate):
         mock_authenticate.return_value = None
         self.client.post('/accounts/login', {'assertion': 'a'})
