b0fc34df66a8802de8d312771986bb6b4c1b9a84
MrLokans <trikster1911@gmail.com>
2015-12-12 12:35:41 +0300

    Added custom User model for auth.

--- superlists/accounts/models.py
+++ superlists/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField()
+    last_login = models.DateTimeField(default=timezone.now)
+    # w/o that hack django auth want work
+    REQUIRED_FIELDS = ()
+    USERNAME_FIELD = 'email'
+
--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cewh@mjx_+*gn+r1-=9lc1n48t5_eu*5@0g16)xqh_+e9-o=#k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [] #['127.0.0.1:8000']
+ALLOWED_HOSTS = []  # ['127.0.0.1:8000']
 
 
 # Application definition
@@ -103,3 +103,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+
+AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = (
+    'accounts.authentication.PersonaAuthenticationBackend',
+    )
